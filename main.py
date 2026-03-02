9a2a1fb59e99e040321c1d5b98aae03292293e64
bernyleog@gmail.com <bernyleog@gmail.com>
2026-03-02 19:52:07 +0800

    changed the import dashboard in main.py

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ if "GOOGLE_CLIENT_SECRET_JSON" in st.secrets and not os.path.exists("client_secr
 # 2. IMPORTS
 import login
 import register
-from FraudShield import dashboard
+from FraudShield.dashboard import show as show_dashboard
+
+# Then use it
+if st.session_state.page == "dashboard":
+    show_dashboard()
 from FraudShield.utils.session import init_session_state, restore_session_from_cookie
 from FraudShield.utils.auth import google_login_or_register
 
